62fcab6358a125e7ea92ba8e4aa6c09fd83d43e4
Leith Hobson <leith@hobson.co.za>
2020-04-05 17:52:45 +0200

    Complete refactor of HTML files to pages directory

--- Server/online.quar/src/main/java/online/quar/application/MvcConfig.java
+++ Server/online.quar/src/main/java/online/quar/application/MvcConfig.java
@@ -9,10 +9,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
-        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/home").setViewName("pages/home");
+        registry.addViewController("/").setViewName("pages/home");
+        registry.addViewController("/hello").setViewName("pages/hello");
+        registry.addViewController("/login").setViewName("pages/login");
     }
 
 }
