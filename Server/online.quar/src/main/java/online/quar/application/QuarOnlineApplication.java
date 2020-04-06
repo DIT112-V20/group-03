1f73bd37ed476cdcccce460647e1a1b8304c463a
Leith Hobson <leith@hobson.co.za>
2020-04-06 12:33:20 +0200

    Refactor Application to be called QuarOnlineApplication

--- Server/online.quar/src/main/java/online/quar/application/QuarOnlineApplication.java
+++ Server/online.quar/src/main/java/online/quar/application/QuarOnlineApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class QuarOnlineApplication {
 
     public static void main(String[] args) throws Throwable {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(QuarOnlineApplication.class, args);
     }
 
 }
