d4601971dd7f932fb936f6e29e5663d631b13a58
Leith Hobson <leith@hobson.co.za>
2020-04-06 13:19:22 +0200

    Implement Logger into main Application class, and add a log entry stating that QuarOnline Application has loaded

--- Server/online.quar/src/main/java/online/quar/application/QuarOnlineApplication.java
+++ Server/online.quar/src/main/java/online/quar/application/QuarOnlineApplication.java
@@ -1,5 +1,6 @@
 package online.quar.application;
 
+import online.quar.application.util.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QuarOnlineApplication {
 
     public static void main(String[] args) throws Throwable {
+        Logger log = Singleton.getLogger();
         SpringApplication.run(QuarOnlineApplication.class, args);
+        log.d("\n\nQuarOnline Application Loaded");
     }
 
 }
