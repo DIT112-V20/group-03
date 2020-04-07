e76f76556d6afe56b355a8f169504f72f4e20cd7
Dia <gusistdi@student.gu.se>
2020-04-07 16:36:27 +0200

    [WebApplication] - Enabled HTTPS protocol with self-signed  certificate.

---
+++ Server/online.quar/src/main/java/online/quar/application/ApplicationSecurity.java
@@ -0,0 +1,13 @@
+//package online.quar.application;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+//
+//@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+//public class ApplicationSecurity {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(ApplicationSecurity.class, args);
+//    }
+//}
