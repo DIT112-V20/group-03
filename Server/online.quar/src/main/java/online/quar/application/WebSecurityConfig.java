0c5dc971ab60bc1cf882c30f89df2193473cb035
Leith Hobson <leith@hobson.co.za>
2020-04-08 18:19:24 +0200

    [WebApplication] - Together with 4df2436 this solves both #30 and #35

--- Server/online.quar/src/main/java/online/quar/application/WebSecurityConfig.java
+++ Server/online.quar/src/main/java/online/quar/application/WebSecurityConfig.java
@@ -1,5 +1,11 @@
 package online.quar.application;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
