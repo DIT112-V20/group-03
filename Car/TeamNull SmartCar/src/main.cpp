1bc8170d2cfbb89709e49eb3853e2e9238570b16
Leith Hobson <leith@hobson.co.za>
2020-04-20 15:12:31 +0200

    [Car] - Demo code

--- Car/TeamNull SmartCar/src/main.cpp
+++ Car/TeamNull SmartCar/src/main.cpp
@@ -1,5 +1,13 @@
 #include "main.hpp"
 
+unsigned long loadTime = 0;
+bool a = false;
+bool b = false;
+bool c = false;
+bool d = false;
+bool e = false;
+bool f = false;
+
 void setup() {
 
     // Open Serial Connection
@@ -21,10 +29,57 @@ void setup() {
     Serial.println("Returned from connectToWiFi");
 
     //Test collision avoidance
-    setDesiredVehicleSpeed(30);
+    // setDesiredVehicleSpeed(30);
+    loadTime = millis();
 }
 
+void movementDemo() {
+  unsigned long previousToggle = 0;
+  int blinkCount = 0;
+
+//   while((blinkCount/2) < toBlink) {
+    unsigned long currentTime = millis();
+
+    //DEMO Code
+    if(currentTime > loadTime + 4000 && !a) {
+        a = true;
+        setDesiredVehicleSpeed(35);
+        setDesireTurnAngle(0);
+    } else if(currentTime > loadTime + 9000 && !b) {
+        b = true;
+        setDesiredVehicleSpeed(33);
+        setDesireTurnAngle(90);
+    } else if(currentTime > loadTime + 13000 && !c) {
+        c = true;
+        setDesiredVehicleSpeed(0);
+        delay(40);
+        setDesiredVehicleSpeed(-35);
+        setDesireTurnAngle(0);
+    } else if(currentTime > loadTime + 15000 && !d) {
+        d = true;
+        setDesiredVehicleSpeed(0);
+        delay(35);
+        setDesiredVehicleSpeed(30);
+        delay(40);
+        setDesiredVehicleSpeed(50);
+        setDesireTurnAngle(-45);
+    } else if(currentTime > loadTime + 16500 && !e) {
+        e = true;
+        setDesiredVehicleSpeed(0);
+        delay(35);
+        setDesiredVehicleSpeed(-30);
+        setDesireTurnAngle(180);
+    } else if(currentTime > loadTime + 18500 && !f) {
+        f = true;
+        setDesiredVehicleSpeed(0);
+    }
+//   }
+//   digitalWrite(LED_BUILTIN, false);
+//   builtinLEDState = false;
+}   
+
 void loop() {
+    movementDemo();
     collisionAvoidance();
     // Serial.print("Front Distance: ");
     // Serial.println(getFrontDistance());
