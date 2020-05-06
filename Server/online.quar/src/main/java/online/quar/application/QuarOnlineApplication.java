bc4fcbbfbe4fac5aabe50bacce3222a5504dbc75
Leith Hobson <leith@hobson.co.za>
2020-05-06 23:30:59 +0200

    [WebApplication] - STOMP API - Implement functionality in carControlApp.js to enable Joystick data to be sent to the server - Finalise joystick implementation in carController.html - Point webSocketTest.html to it's own stompDemoApp.js

--- Server/online.quar/src/main/java/online/quar/application/QuarOnlineApplication.java
+++ Server/online.quar/src/main/java/online/quar/application/QuarOnlineApplication.java
@@ -2,8 +2,14 @@ package online.quar.application;
 
 import online.quar.application.manager.ApplicationManager;
 import online.quar.application.util.Logger;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class QuarOnlineApplication {
@@ -12,8 +18,25 @@ public class QuarOnlineApplication {
         Logger log = Singleton.getLogger();
         ApplicationManager applicationManager = Singleton.getApplicationManager();
         applicationManager.initialiseDatabase();
+
         SpringApplication.run(QuarOnlineApplication.class, args);
+
         log.d("\n\nQuarOnline Application Loaded");
     }
+
+//    @Bean
+//    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+//        return args -> {
+//
+//            System.out.println("Let's inspect the beans provided by Spring Boot:");
+//
+//            String[] beanNames = ctx.getBeanDefinitionNames();
+//            Arrays.sort(beanNames);
+//            for (String beanName : beanNames) {
+//                System.out.println(beanName);
+//            }
+//
+//        };
+//    }
 
 }
--- Server/online.quar/src/main/resources/static/script/carControlApp.js
+++ Server/online.quar/src/main/resources/static/script/carControlApp.js
@@ -1,15 +1,15 @@
 var stompClient = null;
 
 function setConnected(connected) {
-    $("#connect").prop("disabled", connected);
-    $("#disconnect").prop("disabled", !connected);
-    if (connected) {
-        $("#conversation").show();
-    }
-    else {
-        $("#conversation").hide();
-    }
-    $("#greetings").html("");
+    // $("#connect").prop("disabled", connected);
+    // $("#disconnect").prop("disabled", !connected);
+    // if (connected) {
+    //     $("#conversation").show();
+    // }
+    // else {
+    //     $("#conversation").hide();
+    // }
+    // $("#greetings").html("");
 }
 
 function connect() {
@@ -19,6 +19,10 @@ function connect() {
         setConnected(true);
         console.log('Connected: ' + frame);
         stompClient.subscribe('/topic/greetings', function (greeting) {
+            showGreeting(JSON.parse(greeting.body).content);
+        });
+        //TODO: Implement car control object
+        stompClient.subscribe('/topic/car', function (greeting) {
             showGreeting(JSON.parse(greeting.body).content);
         });
     });
@@ -32,19 +36,35 @@ function disconnect() {
     console.log("Disconnected");
 }
 
-function sendName() {
-    stompClient.send("/app/hello", {}, JSON.stringify({'name': $("#name").val()}));
-}
+// function sendName() {
+//     stompClient.send("/app/hello", {}, JSON.stringify({'name': $("#name").val()}));
+// }
+//
+// function showGreeting(message) {
+//     $("#greetings").append("<tr><td>" + message + "</td></tr>");
+// }
 
-function showGreeting(message) {
-    $("#greetings").append("<tr><td>" + message + "</td></tr>");
-}
+// $(function () {
+//     $("form").on('submit', function (e) {
+//         e.preventDefault();
+//     });
+//     $( "#connect" ).click(function() { connect(); });
+//     $( "#disconnect" ).click(function() { disconnect(); });
+//     $( "#send" ).click(function() { sendName(); });
+// });
 
-$(function () {
-    $("form").on('submit', function (e) {
-        e.preventDefault();
-    });
-    $( "#connect" ).click(function() { connect(); });
-    $( "#disconnect" ).click(function() { disconnect(); });
-    $( "#send" ).click(function() { sendName(); });
-});
+function sendJoystickInput(data) {
+
+    //TODO: Real car ID
+    var carId = 1;
+    var carSetSpeed = 0;
+    var carSetAngle = 0;
+
+    if(data != null) {
+        carId = 1;
+        carSetAngle = data.angle.degree > 270? ( - data.angle.degree + 450) : (- data.angle.degree + 90);
+        carSetSpeed = ((-90 < carSetAngle) && (carSetAngle < 90))? data.distance * 2 : -(data.distance * 2);
+    }
+
+    stompClient.send("/app/carControl", {}, JSON.stringify({'carId': carId, 'carSetSpeed': carSetSpeed, 'carSetAngle': carSetAngle}));
+}
