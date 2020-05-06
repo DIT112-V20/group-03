ae661b2e077d9a86fdb9b9971b8fb98522da54af
Leith Hobson <leith@hobson.co.za>
2020-05-07 00:41:02 +0200

    [WebApplication] - Cleanup - Clean up the WSController formatting

--- Server/online.quar/src/main/java/online/quar/application/controller/WSController.java
+++ Server/online.quar/src/main/java/online/quar/application/controller/WSController.java
@@ -21,16 +21,13 @@ public class WSController {
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Greeting greeting(HelloMessage message) throws Exception {
-//        Thread.sleep(1000); // simulated delay
         return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
 
     @MessageMapping("/carControl")
     @SendTo("/topic/car")
     public CarControlInput carControlInput(CarControlInput carControlInput) throws Exception {
-
         return applicationManager.getCarManager().processCarControlInput(carControlInput);
-
     }
 
 }
