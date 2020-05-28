b7c0ae94e3f4c38104be18d7ee897d79fabac570
Altug <gusaltetal@student.gu.se>
2020-05-28 19:45:15 +0200

    [Car] - Unit test class for Car class

---
+++ Server/online.quar/src/test/java/online/quar/application/model/CarTest.java
@@ -0,0 +1,147 @@
+package online.quar.application.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+// @org.junit.jupiter.api.Test
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getSetSpeed() {
+    }
+
+    @Test
+    void setSetSpeed() {
+    }
+
+    @Test
+    void getActualSpeed() {
+    }
+
+    @Test
+    void setActualSpeed() {
+    }
+
+    @Test
+    void getSetAngle() {
+    }
+
+    @Test
+    void setSetAngle() {
+    }
+
+    @Test
+    void getActualAngle() {
+    }
+
+    @Test
+    void setActualAngle() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void setDescription() {
+    }
+
+    @Test
+    void isActive() {
+    }
+
+    @Test
+    void setActive() {
+    }
+
+    @Test
+    void isOnline() {
+    }
+
+    @Test
+    void setOnline() {
+    }
+
+    @Test
+    void isCarObstacleAvoidance() {
+    }
+
+    @Test
+    void setCarObstacleAvoidance() {
+    }
+
+    @Test
+    void isCarCollisionAvoidance() {
+    }
+
+    @Test
+    void setCarCollisionAvoidance() {
+    }
+
+    @Test
+    void getFrontDistance() {
+    }
+
+    @Test
+    void setFrontDistance() {
+    }
+
+    @Test
+    void getLeftFrontDistance() {
+    }
+
+    @Test
+    void setLeftFrontDistance() {
+    }
+
+    @Test
+    void getRightFrontDistance() {
+    }
+
+    @Test
+    void setRightFrontDistance() {
+    }
+
+    @Test
+    void getRearDistance() {
+    }
+
+    @Test
+    void setRearDistance() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
